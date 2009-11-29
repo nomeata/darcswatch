bfc790fddb98a323c6368843b7aeccc6a4bbac08
Joachim Breitner <mail@joachim-breitner.de>
2009-11-29 19:20:10 +0000

    Write diff files again

--- src/Darcs/Watch/GenerateOutput.hs
+++ src/Darcs/Watch/GenerateOutput.hs
@@ -95,10 +95,12 @@ generateOutput config patchNew = do
         
 	putStrLn "Writing output (main page)..."
 	bundleHashes <- listBundles (cData config)
-	patchCount <- S.size <$> mconcat <$> forM bundleHashes (\bundleHash -> do
+
+	patches <- mconcat <$> forM bundleHashes (\bundleHash -> do
 		(ps,_) <- getBundle (cData config) bundleHash
-		return (S.fromList ps)
+		return (M.fromList ps)
 		)
+	let patchCount = M.size patches
 	let bundleCount = length bundleHashes
 	repoData <- forM repos $ \r -> do
 		(bundleHashes,_) <- readBundleList (cData config) (RepositoryBundleList r)
@@ -122,110 +124,16 @@ generateOutput config patchNew = do
 			)
         writeFile (cOutput config ++ "/index.html") $
 		mainPage date nameMapping patchCount bundleCount repoData userData
-{-
-        putStrLn "Reading repositories..."
-	let loadInv rep = do
-                putStr $ "Reading " ++ rep ++ " ... "
-		ps <- readRepository (cData config) rep
-		repoInfo <- getRepositoryInfo (cData config) rep
-		let thisNew = maybe True (>= lastStamp) (lastUpdate repoInfo)
-		putStr (if thisNew then "new data.\n" else " unchanged\n")
-		return (rep, ps, repoInfo, thisNew)
-            readInv (p2r,r2p,r2ri,new) (rep, ps, repoInfo, thisNew) = do
-                let p2r' = foldr (\p -> MM.append p rep) p2r ps
-                    r2p' = MM.extend rep ps r2p :: M.Map RepositoryURL (S.Set PatchInfo)
-		    r2ri' = M.insert rep repoInfo r2ri
-                return (p2r', r2p', r2ri', new || thisNew)
-        (p2r,r2p, r2ri, new) <- foldM readInv (MM.empty, MM.empty, M.empty, patchNew) =<<
-                          sequence (map loadInv (cRepositories config))
-
-        putStrLn "Discovering patch bundles..."
-	bundleHashes <- listBundles (cData config)
-
-	putStrLn $ "Reading patch bundles..."
-        let sortInBundle (u2p, u2rn, p2pe, new) bundleHash = do
-		bundle <- getBundle (cData config) bundleHash
-		let bundleFileName = getBundleFileName (cData config) bundleHash
-		history <- getBundleHistory (cData config) bundleHash
-		
-		let thisNew = any (\(d,_,_) -> d >= lastStamp) history
-
-                let (patches,context) = bundle
-                let u2p' = foldr (\(p,_) -> MM.append (normalizeAuthor (piAuthor p)) p) u2p patches
-                let u2rn' = foldr (\(p,_) ->
-                        M.insertWith (maxBy B.length) (normalizeAuthor (piAuthor p)) (piAuthor p)
-                        ) u2rn patches
-                let p2pe' = foldr (\(p,d) ->
-                        let pe = PatchExtras d context bundleHash bundleFileName history
-                        -- The patch with the smaller context is the more useful
-                        in  M.insertWith (minBy (length.peContext)) p pe
-                        ) p2pe patches
-                return (u2p', u2rn', p2pe', new || thisNew)
-        (u2p, u2rn, p2pe, new') <- foldM sortInBundle (MM.empty, M.empty, M.empty, new) bundleHashes
-        let patches = M.keys p2pe -- Submitted patches
-        let repos   = M.keys r2p -- Repos with patches
-        let users   = M.keys u2p -- Known users
-        
-	if not new' then putStrLn "Nothing new, exiting" else do
-
-
-	-- Clonsider patches as belonging to a repository when either
-	--  * it is already applied
-	--  * all its context is in the repository
-	--  * at least 10 patches of the context are in the repository
-        let addables = do -- List monad
-                patch <- patches
-                repo  <- repos
-                present <- maybeToList $ M.lookup repo r2p
-		if patch `S.member` present
-                  then return (patch,repo)
-		  else do
-			pe <- maybeToList $ M.lookup patch p2pe
-			case partition (`S.member` present) (peContext pe) of
-				(_,[]) -> return (patch,repo)	
-				(m,_) | length m >= 10 -> return (patch,repo)	
-				_ -> []
-
-        -- Patch to possible repos
-        -- Repo to possible patch
-        let p2pr = foldr (\(p,r) -> MM.append p r) MM.empty addables
-        let r2mp = foldr (\(p,r) -> MM.append r p) MM.empty addables
-
-        -- Unmatched patches
-        let unmatched = S.fromList $
-			-- filter (\p -> M.findWithDefault Unmatched p p2s == Unmatched) $
-                        filter (\p -> not (M.member p p2pr)) patches
-
-        now <- getClockTime >>= toCalendarTime
-        let resultData = ResultData p2r r2p u2p p2pe p2pr r2mp r2ri unmatched now u2rn
-	
-	{-
-	putStrLn "Evalutating data"
-	putStrLn (show (length (show (resultData))))
-	-}
-
-        putStrLn "Writing output (main page)..."
-        writeFile (cOutput config ++ "/index.html") (mainPage resultData)
-
-        putStrLn "Writing output (user pages)..."
-        forM_ users $ \u ->
-                writeFile (cOutput config ++ "/" ++ userFile u) (userPage resultData u)
-
-        putStrLn "Writing output (repo pages)..."
-        forM_ repos $ \r ->
-                writeFile (cOutput config ++ "/" ++ repoFile r) (repoPage resultData r)
-
-        putStrLn "Writing output (unmatched patches)..."
-	writeFile (cOutput config ++ "/" ++ "unmatched.html") (unmatchedPage resultData)
 
         putStrLn "Writing output (diffs)..."
-        forM_ patches $ \p -> do
+        forM_ (M.toList patches) $ \(p,d) -> do
 		let filename = cOutput config ++ "/" ++ patchDiffFile p 
 		ex <- doesFileExist filename
 		unless ex $ do
 			putStrLn $ "Writing new patch file " ++ filename
-			B.writeFile filename (peDiff (p2pe M.! p))
+			B.writeFile filename d
 
+{-
         putStrLn "Linking patches"
         let patchLink (p,pe) = do
                 let link = cOutput config ++ "/" ++ patchBasename p ++ ".dpatch"
